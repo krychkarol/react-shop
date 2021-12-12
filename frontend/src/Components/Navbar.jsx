fdf0a1a68139bcb2a1b48140e0758e22d80a1b69
krychkarol <krych.karol.93@gmail.com>
2021-12-12 19:01:34 +0100

    add responsive navbar

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -2,9 +2,27 @@ import React from 'react'
 import SearchIcon from '@mui/icons-material/Search';
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 import PermIdentityIcon from '@mui/icons-material/PermIdentity';
+import MenuIcon from '@mui/icons-material/Menu';
 import Badge from '@mui/material/Badge';
 
 const Navbar = () => {
+
+    const toggleMenu = () => {
+        const menu = document.getElementById('toggle-button');
+        const active = menu.classList.contains('active');
+        const content = document.getElementById('toggle-menu');
+
+        if(active){
+            menu.classList.remove('active');
+            content.style.display = 'none';
+
+        }else{
+            menu.classList.add('active');
+            content.style.display = 'block';
+        }
+    }
+
+
     return (
         <div className='navbar'>
             <div className='wrapper'>
@@ -16,7 +34,10 @@ const Navbar = () => {
                         </div>
                     </div>
                     <div className='center'>
-                        <img src="https://battleofthebeardswi.com/wp-content/uploads/2019/05/8-1.png" alt="#" />
+                        <img src='https://battleofthebeardswi.com/wp-content/uploads/2019/05/8-1.png' alt='#' />
+                        <div className='toggle-btn' onClick={toggleMenu}>
+                            <MenuIcon className='icon' id='toggle-button' />
+                        </div>
                         <div className='title'>
                             BRODACZ
                         </div>
@@ -30,37 +51,78 @@ const Navbar = () => {
                 </div>
                 <div className='bottom'>
                     <div className='category'>
-                        <div className="title">
+                        <div className='title'>
                             Broda
                         </div>
-                        <div className="dropdown-list">
-                            <div className="item">Olejki</div>
-                            <div className="item">Odżywki</div>
-                            <div className="item">Szampony</div>
-                            <div className="item">Farby</div>
+                        <div className='dropdown-list'>
+                            <div className='item'>Olejki</div>
+                            <div className='item'>Odżywki</div>
+                            <div className='item'>Szampony</div>
+                            <div className='item'>Farby</div>
                         </div>
                     </div>
                     <div className='category'>
-                        <div className="title">
+                        <div className='title'>
                             Włosy
                         </div>
-                        <div className="dropdown-list">
-                            <div className="item">Pasty</div>
-                            <div className="item">Pudry</div>
-                            <div className="item">Szampony</div>
-                            <div className="item">Odżywki</div>
-                            <div className="item">Farby</div>
+                        <div className='dropdown-list'>
+                            <div className='item'>Pasty</div>
+                            <div className='item'>Pudry</div>
+                            <div className='item'>Szampony</div>
+                            <div className='item'>Odżywki</div>
+                            <div className='item'>Farby</div>
                         </div>
                     </div>
                     <div className='category'>
-                        <div className="title">
+                        <div className='title'>
                             Akcesoria
                         </div>
-                        <div className="dropdown-list">
-                            <div className="item">Szczotki</div>
-                            <div className="item">Grzebienie</div>
-                            <div className="item">Kartacze</div>
-                            <div className="item">Inne</div>
+                        <div className='dropdown-list'>
+                            <div className='item'>Szczotki</div>
+                            <div className='item'>Grzebienie</div>
+                            <div className='item'>Kartacze</div>
+                            <div className='item'>Inne</div>
+                        </div>
+                    </div>
+                </div>
+
+                {
+                //TOGGLE MENU
+                }
+
+                <div className='toggle-menu' id='toggle-menu'>
+                    <div className='toggle-category'>
+                        <div className='title'>
+                            Broda
+                        </div>
+                        <div className='dropdown-list'>
+                            <div className='item'>Olejki</div>
+                            <div className='item'>Odżywki</div>
+                            <div className='item'>Szampony</div>
+                            <div className='item'>Farby</div>
+                        </div>
+                    </div>
+                    <div className='toggle-category'>
+                        <div className='title'>
+                            Włosy
+                        </div>
+                        <div className='dropdown-list'>
+                            <div className='item'>Pasty</div>
+                            <div className='item'>Pudry</div>
+                            <div className='item'>Szampony</div>
+                            <div className='item'>Odżywki</div>
+                            <div className='item'>Farby</div>
+                        </div>
+                    </div>
+                    <div className='toggle-category'>
+                        <div className='title'>
+                            Akcesoria
+                        </div>
+                        <div className='dropdown-list'>
+                            <div className='item'>Szczotki</div>
+                            <div className='item'>Grzebienie</div>
+                            <div className='item'>Kartacze</div>
+                            <div className='item'>Inne</div>
                         </div>
                     </div>
                 </div>

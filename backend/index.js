0b571a87af3ae947a8c8bb4f52eca6f615d10a5c
krychkarol <krych.karol.93@gmail.com>
2022-01-04 15:53:38 +0100

    api1

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+mongoose
+    .connect("mongodb+srv://admin:admin123@cluster0.8gvmn.mongodb.net/react-shop?retryWrites=true&w=majority")
+    .then(() => console.log("DB Connection Succes"))
+    .catch((err) => console.log(err));
 
 app.listen(5000, () => {
     console.log("Server is running");

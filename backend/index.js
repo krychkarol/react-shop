36e6882e43004fcf66385cb6d24dfcc1fba2bdf2
krychkarol <krych.karol.93@gmail.com>
2022-01-04 16:06:27 +0100

    api2

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
 const app = express();
+dotenv.config();
 
 mongoose
-    .connect("mongodb+srv://admin:admin123@cluster0.8gvmn.mongodb.net/react-shop?retryWrites=true&w=majority")
+    .connect(process.env.MONGO_DB_URL)
     .then(() => console.log("DB Connection Succes"))
     .catch((err) => console.log(err));
 
